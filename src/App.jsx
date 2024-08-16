22e6705ccd0208b7b09d91140d603d33af9797aa
KristinWolv <162977890+KristinWolv@users.noreply.github.com>
2024-08-16 11:57:19 +0200

    Added all components and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,60 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Vans from "./components/pages/Vans/Vans"
+import VanDetail from "./components/pages/Vans/VanDetail";
+import Login from "./components/pages/Login";
+import Dashboard from "./components/pages/Host/Dashboard";
+import Income from "./components/pages/Host/Income";
+import Reviews from "./components/pages/Host/Reviews";
+import HostVans from "./components/pages/Host/HostVans";
+import HostVanInfo from "./components/pages/Host/HostVanInfo";
+import HostVanPhotos from "./components/pages/Host/HostVanPhotos";
+import HostVanPricing from "./components/pages/Host/HostVanPricing";
+import NotFound from "./components/pages/NotFound";
+import HostVanDetail from "./components/pages/Host/HostVanDetail";
+import AuthRequired from "./components/AuthRequired";
+
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
+import "../server";
+ 
+
 
 function App() {
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
+        <Route
+            path="login"
+            element={<Login />}
+          />
+
+
+         <Route element={<AuthRequired />}>
+        <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="income" element={<Income />} />
+        <Route path="reviews" element={<Reviews />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVanDetail />}> 
+        <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} /></Route>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+      </Route>
+      </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
